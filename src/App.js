7995c819eb198c8f01f37fa51e3c7e5ecff10450
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2021-01-24 15:08:40 +0530

    setib up react router

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import ProductList from './Components/ProductList';
 import Detalis from './Components/Detalis';
 import Cart from './Components/Cart';
 import Default from './Components/Default';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
-      <ProductList/>
-      <Detalis/> 
-      <Cart/>
-      <Default/>
+      <Switch>
+        <Route exact path="/" component={ProductList}/>
+        <Route path="/detalis" component={Detalis }/>
+        <Route path="/cart" component={Cart}/>
+        <Route component={Default}/>
+      </Switch>
     </React.Fragment>
   );
 }
 
 export default App;
+
--- src/Components/Default.js
+++ src/Components/Default.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Default extends Component{
     render(){
         return(
-            <h3>Hii. I am VATSAL From DefaultPage</h3>
+            <h3>Hii. I am VATSAL From Page Not Found</h3>
         )
     }
 }
