3cd1332b1f3d0cb0713fa08201e1e8feedfdc60e
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2021-01-24 15:54:35 +0530

    link the page by navigation Link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import logo from '../logo.svg';
 
 export default class Navbar extends Component{
     render(){
         return(
-            <h3>Hii. I am VATSAL From Navbar</h3>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="cart" className="navbar-brand"></img>
+                </Link>
+                <ul className="navbar-nav align-item-center">
+                    <li className="navbar-item ml-5">
+                        <Link to="/" className="nav-link">
+                            PRODUCTS
+                        </Link>
+                    </li>              
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button>
+                        <i className='fas fa-cart-plus btn-lg'/>
+                        MY CARTR
+                    </button>
+                </Link>
+                
+            </nav>
         )
     }
 }
